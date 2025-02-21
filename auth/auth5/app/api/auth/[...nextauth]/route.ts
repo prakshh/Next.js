190e840e8bb950d349f2f11d3cfa74fa04095c8f
prakshh <prakashdas18@gmail.com>
2025-02-21 15:07:04 +0530

    auth correction  - moving to the original

--- auth/auth5/app/api/auth/[...nextauth]/route.ts
+++ auth/auth5/app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,33 @@
 import NextAuth from "next-auth";
-import { AuthOptions } from "next-auth";
-
+// import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-//     }),
-//     FacebookProvider({
-//       clientId: process.env.FACEBOOK_CLIENT_ID ?? "",
-//       clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? "",
-//     }),
-//   ],
-//   pages: {
-//     signIn: "/",
-//   },
-// });
-
-export const authOptions: AuthOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    })
-  ]
-}
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID ?? "",
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? "",
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+});
 
-const handler = NextAuth(authOptions)
+// export const authOptions: AuthOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//     })
+//   ]
+// }
+
+// const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST };
